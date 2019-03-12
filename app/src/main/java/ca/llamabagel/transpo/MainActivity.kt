33da32f74cf5d75e8d593474948f306869e3cc9f
derek <derek@llamabagel.ca>
2019-03-12 10:34:49 -0400

    Initial commit!

---
+++ app/src/main/java/ca/llamabagel/transpo/MainActivity.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2019 Derek Ellis. Subject to the MIT license.
+ */
+
+package ca.llamabagel.transpo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
