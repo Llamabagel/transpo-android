a52e2bb9f0f25a678d85c57bffe5276636fd6f28
derek <derek@llamabagel.ca>
2019-03-25 16:22:05 -0400

    Add base SearchActivity class and files

---
+++ app/src/main/java/ca/llamabagel/transpo/ui/search/SearchActivity.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2019 Derek Ellis. Subject to the MIT license.
+ */
+
+package ca.llamabagel.transpo.ui.search
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ca.llamabagel.transpo.R
+
+class SearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
