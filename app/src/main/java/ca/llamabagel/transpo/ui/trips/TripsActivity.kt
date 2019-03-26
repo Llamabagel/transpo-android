6d7367efd2b391b5d6b0dfe2b4ac7cdbae375c15
derek <derek@llamabagel.ca>
2019-03-25 21:48:04 -0400

    Add base themes Add TripsActivity class, layout, and manifest registration Enable databinding

--- app/src/main/java/ca/llamabagel/transpo/ui/trips/TripsActivity.kt
+++ app/src/main/java/ca/llamabagel/transpo/ui/trips/TripsActivity.kt
@@ -4,3 +4,14 @@
 
 package ca.llamabagel.transpo.ui.trips
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ca.llamabagel.transpo.R
+
+class TripsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trips)
+    }
+}
---
+++ app/src/main/java/ca/llamabagel/transpo/utils/ActivityHelpers.kt
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2019 Derek Ellis. Subject to the MIT license.
+ */
+
+package ca.llamabagel.transpo.utils
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+
+inline fun <reified T : Activity> Activity.startActivity(context: Context) {
+    startActivity(Intent(context, T::class.java))
+}
