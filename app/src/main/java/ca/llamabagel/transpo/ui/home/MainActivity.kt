c4541d82ea7b131826270805ad992f4c7b06c67c
derek <derek@llamabagel.ca>
2019-05-20 23:32:37 -0400

    Add basic toolbar

--- app/src/main/java/ca/llamabagel/transpo/ui/home/MainActivity.kt
+++ app/src/main/java/ca/llamabagel/transpo/ui/home/MainActivity.kt
@@ -4,8 +4,9 @@
 
 package ca.llamabagel.transpo.ui.home
 
-import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -30,6 +31,8 @@ class MainActivity : AppCompatActivity() {
 
         inject(this)
 
+        setSupportActionBar(findViewById(R.id.toolbar))
+
         findViewById<Button>(R.id.button).setOnClickListener {
             viewModel.checkAndApplyDataUpdates()
         }
@@ -51,5 +54,11 @@ class MainActivity : AppCompatActivity() {
                 putExtra(Activities.Trips.EXTRA_STOP_ID, id)
             }
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        MenuInflater(this).inflate(R.menu.main, menu)
+
+        return super.onCreateOptionsMenu(menu)
     }
 }
