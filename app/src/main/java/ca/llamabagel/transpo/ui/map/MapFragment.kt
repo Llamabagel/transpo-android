b17420cba48a9fff840b97d4face5189feca73ce
derek <derek@llamabagel.ca>
2019-05-26 23:12:09 -0400

    Add Mapbox SDK to dependencies Set up simple map in MapFragment

--- app/src/main/java/ca/llamabagel/transpo/ui/map/MapFragment.kt
+++ app/src/main/java/ca/llamabagel/transpo/ui/map/MapFragment.kt
@@ -10,7 +10,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import ca.llamabagel.transpo.BuildConfig
 import ca.llamabagel.transpo.R
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.Style
 
 
 class MapFragment : Fragment() {
@@ -20,6 +24,12 @@ class MapFragment : Fragment() {
     }
 
     private lateinit var viewModel: MapViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Mapbox.getInstance(requireActivity(), BuildConfig.MAPBOX_KEY)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,7 +41,16 @@ class MapFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MapViewModel::class.java)
-        // TODO: Use the ViewModel
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val mapView = view.findViewById<MapView>(R.id.mapView)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync { map ->
+            map.setStyle(Style.TRAFFIC_DAY)
+        }
     }
 
 }
